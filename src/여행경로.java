bcf788e9a80256b72c272ea32218ecdf634294ea
euntaek <dmsxodor2@naver.com>
2021-12-20 16:51:49 +0900

    [프로그래머스 DFS] 여행경로

---
+++ src/여행경로.java
@@ -0,0 +1,19 @@
+public class 여행경로 {
+
+    public static void main(String[] args) {
+        String [][]tickets={{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+        String []answer=solution(tickets);
+
+        System.out.println(answer);
+    }
+
+
+    public static String[] solution(String[][] tickets) {
+        String[] answer = {};
+
+        
+
+        return answer;
+    }
+
+}
